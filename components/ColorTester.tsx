eddaf33d87f800e13fe17d109deadabc1deb79e5
Gaman <gamanfrysta@gmail.com>
2025-12-23 09:40:21 +0800

    Update ColorTester.tsx

--- components/ColorTester.tsx
+++ components/ColorTester.tsx
@@ -49,7 +49,7 @@ const ColorTester: React.FC<ColorTesterProps> = ({ color, hueDef, onSubmit, onSk
       formRef.current?.scrollIntoView({ behavior: "smooth", block: "end" });
     };
     setTimeout(doScroll, 120);
-    setTimeout(doScroll, 400);
+    setTimeout(doScroll, 300);
   };
 
   const handlePrefixClick = (prefix: string) => {
