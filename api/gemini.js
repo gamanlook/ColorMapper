f3da96d4abc41604a716796c111561f3c82440f9
Gaman <gamanfrysta@gmail.com>
2026-02-13 14:02:01 +0800

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -250,7 +250,7 @@ export default async function handler(req) {
     const model = genAI.getGenerativeModel({
      // 使用你指定的模型
       //model: "gemini-2.5-flash-lite",
-      //model: "gemini-3-pro-preview",
+      model: "gemini-3-pro-preview",
       systemInstruction: systemInstructionText,
       generationConfig: {
         responseMimeType: "application/json",
