2f6400a5291ca1ab61d2ecd4e9927ceda1cfe97e
Gaman <gamanfrysta@gmail.com>
2026-04-17 18:21:39 +0800

    Update ColorTester.tsx

--- components/ColorTester.tsx
+++ components/ColorTester.tsx
@@ -606,8 +606,8 @@ const handlePrefixClick = (prefix: string) => {
               <div
                 className="w-full h-full rounded-[1.875rem] opacity-100"
                 style={{
-                  background: `conic-gradient(from var(--angle) at 50% 50%, rgba(255,255,255,0.0) 0deg, rgba(255,255,255,0.0) 69deg, rgba(255,255,255,0.15) 115deg, rgba(255,255,255,0.2) 193deg, rgba(255,255,255,0.0) 270deg, rgba(255,255,255,0.3) 291deg, rgba(255,255,255,0.8) 322deg, rgba(255,255,255,0.0) 1turn)`,
-                  animation: 'rotate-gradient 6s linear infinite',
+                  background: `conic-gradient(from var(--angle) at 50% 50%, rgba(255,255,255,0.0) 0deg, rgba(255,255,255,0.0) 69deg, rgba(255,255,255,0.15) 115deg, rgba(255,255,255,0.2) 193deg, rgba(255,255,255,0.0) 270deg, rgba(255,255,255,0.3) 291deg, rgba(255,255,255,0.5) 322deg, rgba(255,255,255,0.0) 1turn)`,
+                  animation: 'rotate-gradient 7s linear infinite',
                   padding: '1px',
                   WebkitMask: 'linear-gradient(#fff, #fff) content-box, linear-gradient(#fff, #fff)',
                   WebkitMaskComposite: 'xor',
@@ -620,8 +620,8 @@ const handlePrefixClick = (prefix: string) => {
               <div
                 className="w-full h-full rounded-[1.95rem] opacity-70"
                 style={{
-                  background: `conic-gradient(from var(--angle) at 50% 50%, rgba(255,255,255,0.0) 0deg, rgba(255,255,255,0.0) 69deg, rgba(255,255,255,0.15) 115deg, rgba(255,255,255,0.2) 193deg, rgba(255,255,255,0.0) 270deg, rgba(255,255,255,0.3) 291deg, rgba(255,255,255,0.9) 322deg, rgba(255,255,255,0.0) 1turn)`,
-                  animation: 'rotate-gradient 6s linear infinite',
+                  background: `conic-gradient(from var(--angle) at 50% 50%, rgba(255,255,255,0.0) 0deg, rgba(255,255,255,0.0) 69deg, rgba(255,255,255,0.15) 115deg, rgba(255,255,255,0.2) 193deg, rgba(255,255,255,0.0) 270deg, rgba(255,255,255,0.3) 291deg, rgba(255,255,255,0.6) 322deg, rgba(255,255,255,0.0) 1turn)`,
+                  animation: 'rotate-gradient 7s linear infinite',
                   padding: '2.5px',
                   WebkitMask: 'linear-gradient(#fff, #fff) content-box, linear-gradient(#fff, #fff)',
                   WebkitMaskComposite: 'xor',
