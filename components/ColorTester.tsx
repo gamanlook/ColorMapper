7a3d9a15ee373f085b3d657bbad95f1844d748f3
Gaman <gamanfrysta@gmail.com>
2026-04-17 17:31:27 +0800

    Update ColorTester.tsx

--- components/ColorTester.tsx
+++ components/ColorTester.tsx
@@ -604,7 +604,7 @@ const handlePrefixClick = (prefix: string) => {
                   className="w-full h-full rounded-[1.875rem] opacity-100"
                   style={{
                     background: `conic-gradient(from var(--angle) at 50% 50%, rgba(255,255,255,0.0) 0deg, rgba(255,255,255,0.0) 69deg, rgba(255,255,255,0.15) 115deg, rgba(255,255,255,0.2) 193deg, rgba(255,255,255,0.0) 270deg, rgba(255,255,255,0.3) 291deg, rgba(255,255,255,0.8) 322deg, rgba(255,255,255,0.0) 1turn)`,
-                    animation: 'rotate-gradient 8s linear infinite',
+                    animation: 'rotate-gradient 6s linear infinite',
                     padding: '1px',
                     WebkitMask: 'linear-gradient(#fff, #fff) content-box, linear-gradient(#fff, #fff)',
                     WebkitMaskComposite: 'xor',
@@ -618,7 +618,7 @@ const handlePrefixClick = (prefix: string) => {
                   className="w-full h-full rounded-[1.95rem] opacity-70"
                   style={{
                     background: `conic-gradient(from var(--angle) at 50% 50%, rgba(255,255,255,0.0) 0deg, rgba(255,255,255,0.0) 69deg, rgba(255,255,255,0.15) 115deg, rgba(255,255,255,0.2) 193deg, rgba(255,255,255,0.0) 270deg, rgba(255,255,255,0.3) 291deg, rgba(255,255,255,0.9) 322deg, rgba(255,255,255,0.0) 1turn)`,
-                    animation: 'rotate-gradient 8s linear infinite',
+                    animation: 'rotate-gradient 6s linear infinite',
                     padding: '2.5px',
                     WebkitMask: 'linear-gradient(#fff, #fff) content-box, linear-gradient(#fff, #fff)',
                     WebkitMaskComposite: 'xor',
