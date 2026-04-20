d8a68c91a19fee587a7a47f721aec7c45f604cd9
Gaman <gamanfrysta@gmail.com>
2026-04-20 13:59:25 +0800

    Update ColorTester.tsx

--- components/ColorTester.tsx
+++ components/ColorTester.tsx
@@ -615,12 +615,12 @@ const handlePrefixClick = (prefix: string) => {
                 }}
               />
             </div>
-            {/* Bloom (masked to be just a ring, using a -16px buffer zone to prevent Safari blur clipping) */}
-            <div className="absolute -inset-[16px] z-[0] pointer-events-none blur-[4px]">
+            {/* Bloom (masked to be just a ring) */}
+            <div className="absolute -inset-[1px] z-[0] pointer-events-none blur-[5px]">
               <div
-                className="absolute inset-[15px] w-[calc(100%-30px)] h-[calc(100%-30px)] rounded-[1.95rem] opacity-100"
+                className="w-full h-full rounded-[1.95rem] opacity-100"
                 style={{
-                  background: `conic-gradient(from var(--angle) at 50% 50%, rgba(255,255,255,0.0) 0deg, rgba(255,255,255,0.05) 40deg, rgba(255,255,255,0.2) 90deg, rgba(255,255,255,0.05) 140deg, rgba(255,255,255,0.0) 180deg, rgba(255,255,255,0.05) 220deg, rgba(255,255,255,0.1) 250deg, rgba(255,255,255,0.1) 290deg, rgba(255,255,255,0.05) 330deg, rgba(255,255,255,0.0) 1turn)`,
+                  background: `conic-gradient(from var(--angle) at 50% 50%, rgba(255,255,255,0.0) 0deg, rgba(255,255,255,1.0) 40deg, rgba(255,255,255,1.0) 90deg, rgba(255,255,255,1.0) 140deg, rgba(255,255,255,0.0) 180deg, rgba(255,255,255,1.0) 220deg, rgba(255,255,255,1.0) 250deg, rgba(255,255,255,1.0) 290deg, rgba(255,255,255,1.0) 330deg, rgba(255,255,255,0.0) 1turn)`,
                   animation: 'rotate-gradient 8s linear infinite',
                   padding: '2.5px',
                   WebkitMask: 'linear-gradient(#fff, #fff) content-box, linear-gradient(#fff, #fff)',
